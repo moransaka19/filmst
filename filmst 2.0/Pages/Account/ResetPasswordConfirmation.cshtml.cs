eab10e875eabcc1b5e1f05730bb4106dbf8b2c4e
Guilegaton <yulii.vedkal@nure.ua>
2018-04-09 14:59:10 +0300

    Remake project filmst

---
+++ filmst 2.0/Pages/Account/ResetPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace filmst_2._0.Pages.Account
+{
+    public class ResetPasswordConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
