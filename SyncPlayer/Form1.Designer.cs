96594cc83859bc1e829f9e907a9e8ed03175393b
Несмиян Дмитрий Михайлович <nesmiyandima@mail.ru>
2018-04-20 10:05:01 +0300

    Created an WinForm app. Added folders and files chosing.

--- SyncPlayer/Form1.Designer.cs
+++ SyncPlayer/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWMP = new AxWMPLib.AxWindowsMediaPlayer();
+            this.ChFileBTN = new System.Windows.Forms.Button();
+            this.RunBTN = new System.Windows.Forms.Button();
+            this.PathTB = new System.Windows.Forms.TextBox();
+            this.PlaylistLB = new System.Windows.Forms.ListBox();
+            this.ChPlLstBTN = new System.Windows.Forms.Button();
+            this.ChFoldBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWMP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWMP
+            // 
+            this.axWMP.Enabled = true;
+            this.axWMP.Location = new System.Drawing.Point(12, 41);
+            this.axWMP.Name = "axWMP";
+            this.axWMP.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWMP.OcxState")));
+            this.axWMP.Size = new System.Drawing.Size(550, 397);
+            this.axWMP.TabIndex = 0;
+            // 
+            // ChFileBTN
+            // 
+            this.ChFileBTN.Location = new System.Drawing.Point(383, 12);
+            this.ChFileBTN.Name = "ChFileBTN";
+            this.ChFileBTN.Size = new System.Drawing.Size(75, 23);
+            this.ChFileBTN.TabIndex = 1;
+            this.ChFileBTN.Text = "Choose File";
+            this.ChFileBTN.UseVisualStyleBackColor = true;
+            this.ChFileBTN.Click += new System.EventHandler(this.ChFileBTN_Click);
+            // 
+            // RunBTN
+            // 
+            this.RunBTN.Location = new System.Drawing.Point(464, 12);
+            this.RunBTN.Name = "RunBTN";
+            this.RunBTN.Size = new System.Drawing.Size(98, 23);
+            this.RunBTN.TabIndex = 2;
+            this.RunBTN.Text = "Run this track";
+            this.RunBTN.UseVisualStyleBackColor = true;
+            this.RunBTN.Click += new System.EventHandler(this.RunBTN_Click);
+            // 
+            // PathTB
+            // 
+            this.PathTB.Location = new System.Drawing.Point(12, 14);
+            this.PathTB.Name = "PathTB";
+            this.PathTB.Size = new System.Drawing.Size(365, 20);
+            this.PathTB.TabIndex = 3;
+            // 
+            // PlaylistLB
+            // 
+            this.PlaylistLB.Location = new System.Drawing.Point(568, 12);
+            this.PlaylistLB.Name = "PlaylistLB";
+            this.PlaylistLB.Size = new System.Drawing.Size(220, 381);
+            this.PlaylistLB.TabIndex = 4;
+            this.PlaylistLB.SelectedIndexChanged += new System.EventHandler(this.PlaylistLB_SelectedIndexChanged);
+            // 
+            // ChPlLstBTN
+            // 
+            this.ChPlLstBTN.Location = new System.Drawing.Point(568, 399);
+            this.ChPlLstBTN.Name = "ChPlLstBTN";
+            this.ChPlLstBTN.Size = new System.Drawing.Size(95, 23);
+            this.ChPlLstBTN.TabIndex = 5;
+            this.ChPlLstBTN.Text = "Choose Playlist";
+            this.ChPlLstBTN.UseVisualStyleBackColor = true;
+            this.ChPlLstBTN.Click += new System.EventHandler(this.ChPlLstBTN_Click);
+            // 
+            // ChFoldBTN
+            // 
+            this.ChFoldBTN.Location = new System.Drawing.Point(669, 399);
+            this.ChFoldBTN.Name = "ChFoldBTN";
+            this.ChFoldBTN.Size = new System.Drawing.Size(75, 23);
+            this.ChFoldBTN.TabIndex = 6;
+            this.ChFoldBTN.Text = "Choose Fold";
+            this.ChFoldBTN.UseVisualStyleBackColor = true;
+            this.ChFoldBTN.Click += new System.EventHandler(this.ChFoldBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChFoldBTN);
+            this.Controls.Add(this.ChPlLstBTN);
+            this.Controls.Add(this.PlaylistLB);
+            this.Controls.Add(this.PathTB);
+            this.Controls.Add(this.RunBTN);
+            this.Controls.Add(this.ChFileBTN);
+            this.Controls.Add(this.axWMP);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWMP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWMP;
+        private System.Windows.Forms.Button ChFileBTN;
+        private System.Windows.Forms.Button RunBTN;
+        private System.Windows.Forms.TextBox PathTB;
+        private System.Windows.Forms.ListBox PlaylistLB;
+        private System.Windows.Forms.Button ChPlLstBTN;
+        private System.Windows.Forms.Button ChFoldBTN;
     }
 }
 
--- SyncPlayer/Form1.cs
+++ SyncPlayer/Form1.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SyncPlayer
 {
     public partial class Form1 : Form
     {
+        //[System.Runtime.InteropServices.DllImport("winmm.dll")]
+        //private static extern Boolean PlaySound(string lpszName, int hmodule, int dwflags);
+        
         public Form1()
         {
             InitializeComponent();
+        }
+        string[] paths;
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+        
+        private void ChFileBTN_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3|" +
+                                     "mp4 files (*.mp4)|*.mp4|" +
+                                     "avi files (*.avi)|*.avi|" +
+                                     "ogg files (*.ogg)|*.ogg|" +
+                                     "wmv files (*.wmv)|*.wmv";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                PathTB.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void RunBTN_Click(object sender, EventArgs e)
+        {
+            axWMP.URL = PathTB.Text;
+            axWMP.Ctlcontrols.play();
+        }
+
+        private void PlaylistLB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWMP.URL = paths[PlaylistLB.SelectedIndex];
+        }
+
+        private void ChFoldBTN_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                paths = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                foreach (string path in paths)
+                {
+                    PlaylistLB.Items.Add(Path.GetFileName(path));
+                }
+            }
+        }
+
+        private void ChPlLstBTN_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                paths = openFileDialog1.FileNames;
+                foreach (string path in paths)
+                {
+                    PlaylistLB.Items.Add(Path.GetFileName(path));
+                }
+            }
         }
     }
 }
